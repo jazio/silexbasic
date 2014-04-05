722b1212e2b8a0d1ec970d44158b78688839bfa1
Ovi Farcas <farcaso@gmail.com>
2014-04-05 16:55:16 +0200

    First commit

---
+++ web/index.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+// Register Service Providers
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+// Controllers
+$app->get('/', function () use ($app) {
+    return 'Homepage';
+}
+);
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello ' . $app->escape($name);
+}
+);
+
+$app->get('/hellotwig/{name}', function ($name) use ($app) {
+    return $app['twig']->render('hello.twig', array(
+    		'name' => $name,
+    	));
+});
+//var_dump($app);
+$app->run();
