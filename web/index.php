a593e3cea806146532687b3a2423b153b7d399ee
Ovi Farcas <farcaso@gmail.com>
2014-04-05 18:53:01 +0200

    Add sample blog controllers

--- web/index.php
+++ web/index.php
@@ -1,16 +1,23 @@
 <?php
+/**
+ * author: Ovi Farcas
+ * website: http://www.jazio.net
+ */
+/*============ Dependencies ============*/
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
-// Register Service Providers
+/*============ Register Service Providers ============*/
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
-// Controllers
+/*============ Controllers ============*/
+
+// Homepage
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.twig',array(
     'welcome' => $welcome, // not set yet
@@ -18,15 +25,61 @@ $app->get('/', function () use ($app) {
 }
 );
 
+// Hello without template
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello ' . $app->escape($name);
 }
 );
 
+// Hello with template
 $app->get('/hellotwig/{name}', function ($name) use ($app) {
     return $app['twig']->render('hello.html', array(
-    		'name' => $name,
-    	));
+            'name' => $name,
+        ));
 });
+
+// Blog Post Overview
+/* The below array could be fetched from a json file or database*/
+$blogPosts = array(
+    1 => array(
+        'date'   => '03-03-2014',
+        'author' => 'Andrei Plesu',
+        'title'  => 'Despre frumusetea uitata a vietii',
+        'body'   => 'Lore ipsum1',
+        ),
+    2 => array(
+        'date'   => '02-04-2014',
+        'author' => 'Gabriel Liiceanu',
+        'title'  => 'Jurnal de la Paltinis',
+        'body'   => 'Lore ipsum2',
+        ),
+    );
+
+$app->get('/blog', function () use ($blogPosts) {
+    $output = '';
+    foreach ($blogPosts as $blog) {
+        $output .= '<h2>'.$blog['title'].'</h2>';
+        $output .= '<i>'.$blog['author'].'</i>'.' ' .'<small>'.$blog['date'].'</small>';
+        $output .= '<p>'.$blog['body'].'</p>';
+    }
+    return $output;
+}
+);
+// Blog Post Overview with Twig Template
+
+$app->get('/blog/{id}', function ($id) use ($blogPosts, $app) {
+    return $app['twig']->render('blog.twig',array(
+        'title' => $blogPosts[$id]['title'],
+        'author' => $blogPosts[$id]['author'],
+        'date' => $blogPosts[$id]['date'],
+        'body' => $blogPosts[$id]['body'],
+        ));
+    
+}
+);
 //var_dump($app);
+//@todo
+///blog/id
+//header redirect
+//404
 $app->run();
