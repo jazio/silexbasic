fcc5656016ada8789e11d1c7a3ed7d0780cafa89
Ovi Farcas <farcaso@gmail.com>
2014-04-26 13:07:56 +0200

    Init config files

---
+++ config/dev.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Dev -- optional file to be included during development phase
+ */
+// Convert PHP Errors into Exceptions
+// Read here why http://silex.sensiolabs.org/doc/cookbook/error_handler.html
+use Symfony\Component\Debug\ErrorHandler;
+
+// Include the prod configuration
+require __DIR__.'/prod.php';
+
+// Enable PHP Error level
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+
+// Enable debug mode
+$app['debug'] = true;
+
+// Handle fatal errors
+ErrorHandler::register();
+
+
+
+
+// @TODO -- STUDY PROFILER
+/* 
+use Silex\Provider\WebProfilerServiceProvider;
+
+$app->register($p = new WebProfilerServiceProvider(), array(
+    'profiler.cache_dir' => __DIR__.'/../cache/profiler',
+));
+$app->mount('/_profiler', $p);
+*/
+
+
+
+// @TODO -- STUDY ENABLING MONOLOG LOGS
+/*
+use Silex\Provider\WebProfilerServiceProvider;
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../logs/silex_dev.log',
+));
+*/
