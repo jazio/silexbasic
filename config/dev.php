820ca321d09a3b54921ad4a5f53bd6ed2b49359f
Ovi Farcas <farcaso@gmail.com>
2014-04-26 23:48:15 +0200

    Split Main Application File

--- config/dev.php
+++ config/dev.php
@@ -6,19 +6,22 @@
 // Convert PHP Errors into Exceptions
 // Read here why http://silex.sensiolabs.org/doc/cookbook/error_handler.html
 use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\Debug;
 
 // Include the prod configuration
 require __DIR__.'/prod.php';
 
 // Enable PHP Error level
 error_reporting(E_ALL);
-ini_set('display_errors','On');
+//ini_set('display_errors',1);
 
 // Enable debug mode
 $app['debug'] = true;
 
 // Handle fatal errors
 ErrorHandler::register();
+
+Debug::enable();
 
 
 
